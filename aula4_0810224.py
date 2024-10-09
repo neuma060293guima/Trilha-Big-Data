24bfc84999282baa5f427ae40d8489787339fb12
neuma <neuma.lucena@yahoo.com.br>
2024-10-08 21:12:55 -0300

    Add files via upload

---
+++ aula4_0810224.py
@@ -0,0 +1,34 @@
+###ESTRUTURA DE CONDIÇÃO###
+
+#IF ELSE X SWUITCH CASE
+
+#1) IF ELSE
+mes=int (input('Digite um nímero entre 1 a 12:') )
+
+if mes==1:
+    print('janeiro')
+elif mes==2:
+    print('Fevereiro')
+elif mes==3:
+    print('Março')
+elif mes==4:
+    print('Abril')
+elif mes==5:
+    print('Maio')
+elif mes==6:
+    print('Junho')
+elif mes==7:
+    print('Julho')
+elif mes==8:
+    print('Agosto')
+elif mes==9:
+    print('Setembro')
+elif mes==10:
+    print('Outubro')
+elif mes==11:
+    print('Novembro')
+elif mes==12:
+    print('Dezembro')
+else: 
+    print("Por favor forneça um número válido!")
+    mes=int (input('Digite um nímero entre 1 a 12:') )
